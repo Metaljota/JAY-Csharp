ee842de06a13110b9a6a59e318756d0f94c224f8
Jay <105905548+Metaljota@users.noreply.github.com>
2022-07-17 14:57:57 +1000

    New Csharp code

---
+++ Assessment 2/Driver.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Assessment_2
+{
+    class Driver
+    {
+        public int licence;
+        public string fName;
+        public string lName;
+        public int phone;
+        public string[] states;
+        public string[] address;
+        public int totalDemeritPoints;
+        public static int MaxDemeritPoints = 12;
+        
+        public Driver(int licence, string fName, string lName, int phone, string[] states, string[] address, int totalDemeritPoints, int MaxDemeritPoints = 12)
+        {
+            this.licence = licence;
+            this.fName = fName;
+            this.lName = lName;
+            this.phone = phone;
+            this.address = address;
+            this.states = states;
+            this.totalDemeritPoints = totalDemeritPoints;
+        
+
+        }
+
+
+        public void AddDemeritPoints(int newDP)
+        {
+            totalDemeritPoints = newDP + totalDemeritPoints;
+
+            if (totalDemeritPoints >= 9 && totalDemeritPoints < MaxDemeritPoints)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("LICENCE ESTATUS WARNING");
+                Console.WriteLine("Total Demerit points: " + totalDemeritPoints);
+                Console.WriteLine("License suspension is imminent");
+                Console.WriteLine(" ");
+            }
+            else if (totalDemeritPoints < 0)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("LICENCE ESTATUS WARNING");
+                Console.WriteLine("Total Demerit points: " + totalDemeritPoints);
+                Console.WriteLine("ERROR");
+                Console.WriteLine(" ");
+                totalDemeritPoints = 0;
+            }
+            else if (totalDemeritPoints > MaxDemeritPoints)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("LICENCE ESTATUS WARNING");
+                Console.WriteLine("Total Demerit points: " + totalDemeritPoints);
+                Console.WriteLine("Demerit points is over the limit");
+                Console.WriteLine(" ");
+                totalDemeritPoints = 0;
+            }
+
+        }
+
+        public void PrintDriver()
+        {
+  
+            Console.WriteLine("The driver " + fName + " " + lName + ", has a driver licence number: " + licence + " Contact phone number: " + phone + " Demerit points: " + totalDemeritPoints);
+            Console.WriteLine(" ");
+            Console.WriteLine("The driver is licenced to drive in the following states: ");
+            foreach (string state in states )
+            {
+                Console.Write(state);
+                
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine("Driver address: ");
+            foreach (string item in address)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+        public void DriverFile()
+        {
+            string driverDetails = "The driver " + fName + " " + lName + ", has a driver licence number: " + licence + " Contact phone number: " + phone + " Demerit points: " + totalDemeritPoints;
+            File.WriteAllText("Driver.txt", driverDetails);
+
+            string read = File.ReadAllText("Driver.txt");
+            Console.WriteLine(read);
+        }
+
+    }
+}
---
+++ Assessment 2/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+
+
+namespace Assessment_2
+{
+    //----------------------------------VEHICLE-------------------------------------
+
+    class Vehicle 
+    {
+        public int rego;
+        public string make;
+        public string model;
+        public int totalKm;
+        Driver theDriver;
+        public Vehicle(int rego, string make, string model, int totalKm, Driver theDriver)
+        {
+            this.rego = rego;
+            this.make = make;
+            this.model = model;
+            this.totalKm = totalKm;
+            this.theDriver = theDriver; 
+        }
+
+        public void UpDateKm(int newkm)
+        {
+            totalKm = newkm + totalKm; 
+        }
+      
+        public void PrintVehicle()
+        {
+            Console.WriteLine("Additional details: ");
+            Console.WriteLine("Rego is: " + rego + "." + " Make is: " + make + "." + " Model is: " + model + "." + " Km's are: " + totalKm + "km.");
+        }
+
+    }
+
+    //----------------------------------CAR-------------------------------------
+
+    class Car : Vehicle
+    {
+        public string bodyType;
+        public string colour;
+        public string upholstery;
+        public int doors;
+        public Car(string bodyType, string colour, string upholstery, int doors, int rego, string make, string model, int totalKm, Driver theDriver)
+            : base(rego, make, model, totalKm, theDriver)
+        {
+            this.bodyType = bodyType;
+            this.colour = colour;
+            this.upholstery = upholstery;
+            this.doors = doors;
+        }
+
+        public void ChangeColor(string newColor)
+        {
+            colour = newColor;
+        }
+
+        public void PrintCar()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("Car details: ");
+            Console.WriteLine("Body type: " + bodyType + ", colour: " + colour + ", The upholstery: " + upholstery + ", Doors number: " + doors);
+        }
+        
+    }
+
+    //-----------------------------TRUCK-------------------------------------------
+
+    class Truck : Vehicle
+    {
+        public int load;
+        public int axles;
+        public int wheels;
+        public Truck(int load, int axles, int wheels, int rego, string make, string model, int totalKm, Driver theDriver)
+            : base(rego, make, model, totalKm, theDriver)
+        {
+            this.load = load;
+            this.axles = axles;
+            this.wheels = wheels;
+        }
+
+
+        public void PrintTruck()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("Truck details: ");
+            Console.WriteLine("Maximun load: " + load + " Number of axles: " + axles + " Number of wheels: " + wheels);
+        }
+    }
+
+   
+    //-------------------------------------------------------------------------
+    //-----------------------------PROGRAM--------------------------------------
+    //--------------------------------------------------------------------------
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] address1 = { "Street: 24 Lincoln Road", "City : Melbourne", "State : Victoria ", "Postcode : 3040" };
+            string[] states1 = { "Qld :"," Vic :", " NSW", };
+            Driver driver1 = new Driver(58946, "Jack", "Sparrow", 058423645, states1, address1, 5);
+            driver1.AddDemeritPoints(0);
+            driver1.PrintDriver();
+
+            Console.WriteLine(" "); //Write and read the driver file
+            Console.WriteLine("Checking driver file".ToUpper());
+            driver1.DriverFile();
+
+            Car car1 = new Car("sedan", "red", "plastic", 4, 978564, "Ford", "Mustang", 2300, driver1);
+            car1.UpDateKm(500);
+            car1.ChangeColor("blue");
+            car1.PrintCar();
+            car1.PrintVehicle();
+
+            Truck truck1 = new Truck(200, 5, 6, 878969, "Mercedes", "Diesel", 1500, driver1);
+            truck1.PrintTruck();
+            truck1.PrintVehicle();
+
+            Console.WriteLine(" ");
+            Console.WriteLine("==========================================");
+            Console.WriteLine("Another Driver".ToUpper());
+            Console.WriteLine("==========================================");
+
+            string[] address2 = { "Street: 15 Sydney road", "City: Melbourne", "State: Victoria", "Postcode: 2740" };
+            string[] states2 = { "SA :", " VIC :" };
+            Driver driver2 = new Driver(258963, "Pedro", "Valdez", 0426897456, states2, address2, 5);
+            driver2.AddDemeritPoints(6);
+            driver2.PrintDriver();
+
+            Car car2 = new Car("Suv", "black", "leather", 4, 426789, "Toyota", "RAV4", 15000, driver2);
+            car2.UpDateKm(700);
+            car2.ChangeColor("green");
+            car2.PrintCar();
+            car2.PrintVehicle();
+
+            Truck truck2 = new Truck(185, 5, 6, 123456, "Mercedes", "The Actros", 23465, driver2);
+            truck2.UpDateKm(150);
+            truck2.PrintTruck();
+            truck2.PrintVehicle();
+
+
+
+        }
+    }
+}
